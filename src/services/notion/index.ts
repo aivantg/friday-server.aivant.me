1448d0d709cc16b654621736e12bec3fa1e3fbad
Aivant Goyal <aivant@watershedclimate.com>
2023-07-07 23:48:50 -0500

    Added new person, add note, and getNames routes

--- src/services/notion/index.ts
+++ src/services/notion/index.ts
@@ -5,11 +5,20 @@ import {
   PageObjectResponse,
   PartialPageObjectResponse,
 } from '@notionhq/client/build/src/api-endpoints';
-import { getAllPeople } from './peopleUtils';
+import {
+  addNewPerson,
+  addNoteToPerson,
+  findPersonIdByName,
+  getAllPeople,
+} from './peopleUtils';
 
 /**
  * This application is a layer over the Notion API to help facilitate
  * useful shortcuts and integrations.
+ *
+ * Useful api documentation:
+ * - https://github.com/makenotion/notion-sdk-js
+ * - https://developers.notion.com/docs
  */
 
 if (!process.env.NOTION_TOKEN) {
@@ -23,19 +32,113 @@ if (!process.env.NOTION_TOKEN) {
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const router = express.Router();
 
-// Index: shows all routes
-// TODO: implement
-router.get('/', async (req, res) => {
-  res.status(200).json({ message: 'Hello Notion API!' });
-});
-
-// Use the notion JS sdk to query all values from users database
-// Return json array with each row's properties
+/**
+ * GET: /people
+ * Returns JSON array of all people in people database
+ * Includes properties: name, location, notes
+ */
 router.get('/people', async (req, res) => {
   try {
     res.send(await getAllPeople(notion));
   } catch (error) {
     res.status(400).json(error);
+  }
+});
+
+/**
+ * GET: /peopleNames
+ * Returns JSON array of people names in people database
+ */
+router.get('/peopleNames', async (req, res) => {
+  try {
+    const people = await getAllPeople(notion);
+    res.send(people.map((p) => p.name).join(','));
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+/**
+ * GET: /addPerson
+ * Adds new person row to people database
+ * Pulls out the following properties out of the query parameter:
+ * - name
+ * - location (multiple separated by commas)
+ * - notes (optiona)
+ */
+router.get('/addPerson', async (req, res) => {
+  try {
+    const { name, location, notes, tags } = req.query;
+    if (!name || !location) {
+      res
+        .status(400)
+        .send('Missing one or more required parameters: name, location');
+    }
+
+    res.send(
+      await addNewPerson(notion, {
+        name: name as string,
+        location: location as string,
+        notes: notes ? (notes as string) : '',
+        tags: tags ? (tags as string) : '',
+      })
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(JSON.stringify(error));
+  }
+});
+
+/**
+ * GET: /findPerson
+ * Finds person by name and returns their id
+ * If multiple people are found, picks first
+ * If no people are found, throws error
+ */
+router.get('/findPerson', async (req, res) => {
+  try {
+    const { name } = req.query;
+    if (!name) {
+      res.status(400).send('Missing required parameter: name');
+    }
+
+    const personId = await findPersonIdByName(notion, name as string);
+    if (!personId) {
+      res.status(400).send(`No person found with name: ${name}`);
+    }
+
+    res.send(personId);
+  } catch (error) {
+    res.status(400).send(JSON.stringify(error));
+  }
+});
+
+/**
+ * GET: /addNoteToPerson
+ * Adds note to person with given name. Note is added as children of person's page
+ * If no person is found, throws error
+ * If no note is provided, throws error
+ * If note is added successfully, returns link to new block.
+ */
+router.get('/addNoteToPerson', async (req, res) => {
+  try {
+    const { name, note } = req.query;
+    if (!name || !note) {
+      res
+        .status(400)
+        .send('Missing one or more required parameters: name, note');
+      return;
+    }
+
+    const personId = await findPersonIdByName(notion, name as string);
+    if (!personId) {
+      res.status(400).send(`No person found with name: ${name}`);
+      return;
+    }
+
+    res.send(await addNoteToPerson(notion, personId as string, note as string));
+  } catch (error) {
+    res.status(400).send(JSON.stringify(error));
   }
 });
 
--- src/services/notion/peopleUtils.ts
+++ src/services/notion/peopleUtils.ts
@@ -1,14 +1,22 @@
 import { Client, collectPaginatedAPI } from '@notionhq/client';
+import {
+  AppendBlockChildrenResponse,
+  CreatePageResponse,
+  PageObjectResponse,
+  search,
+} from '@notionhq/client/build/src/api-endpoints';
 
 // Mapping of database type to notion database id.
 // This info isn't secret as nothing can be done with it if the database isn't public.
 const PEOPLE_DATABASE = 'a41b62454dba47f09b18cc3f6543938f';
+const BASE_URL = 'https://www.notion.so';
 
 export type Person = {
   id: string;
   name: string;
   notes: string;
   location: string;
+  tags: string;
 };
 
 type PersonRow = {
@@ -17,8 +25,97 @@ type PersonRow = {
     Name: { title: { plain_text: string } };
     Notes?: { rich_text: [{ plain_text: string }] };
     Location: { multi_select: [{ name: string }] };
+    Tags?: { multi_select: [{ name: string }] };
   };
 };
+
+export async function findPersonIdByName(
+  notion: Client,
+  nameToSearch: string
+): Promise<string | undefined> {
+  const searchResults: PersonRow[] = (await collectPaginatedAPI(
+    notion.databases.query,
+    {
+      database_id: PEOPLE_DATABASE,
+      filter: {
+        property: 'Name',
+        rich_text: {
+          contains: nameToSearch,
+        },
+      },
+    }
+  )) as unknown as PersonRow[];
+
+  return searchResults[0]?.id;
+}
+
+// Append bulleted list item block to page with id: `personId`
+export async function addNoteToPerson(
+  notion: Client,
+  personId: string,
+  note: string
+): Promise<string> {
+  const newBlock: any = await notion.blocks.children.append({
+    block_id: personId,
+    children: [
+      {
+        object: 'block',
+        type: 'bulleted_list_item',
+        bulleted_list_item: {
+          rich_text: [
+            {
+              type: 'text',
+              text: { content: note },
+            },
+          ],
+        },
+      },
+    ],
+  });
+  const resultId = newBlock.results[0].id.split('-').join('');
+  console.log(resultId);
+  return `${BASE_URL}/${personId.split('-').join('')}#${resultId}`;
+}
+
+export async function addNewPerson(notion: Client, person: Omit<Person, 'id'>) {
+  const result = (await notion.pages.create({
+    parent: {
+      type: 'database_id',
+      database_id: PEOPLE_DATABASE,
+    },
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: person.name,
+            },
+          },
+        ],
+      },
+      Notes: {
+        rich_text: [
+          {
+            text: {
+              content: person.notes,
+            },
+          },
+        ],
+      },
+      Location: {
+        multi_select: person.location.split(',').map((s) => {
+          return { name: s.trim() };
+        }),
+      },
+      Tags: {
+        multi_select: person.tags.split(',').map((s) => {
+          return { name: s.trim() };
+        }),
+      },
+    },
+  })) as PageObjectResponse;
+  return result.url;
+}
 
 export async function getAllPeople(notion: Client): Promise<Person[]> {
   const allPersonRows: PersonRow[] = (await collectPaginatedAPI(
@@ -40,6 +137,9 @@ export async function getAllPeople(notion: Client): Promise<Person[]> {
       location: personRow.properties.Location.multi_select
         .map((s) => s.name)
         .join(', '),
+      tags: personRow.properties.Tags?.multi_select
+        .map((s) => s.name)
+        .join(','),
     };
   });
 }
